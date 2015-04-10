49a5c99135665566ed307eb073b9bc1424b2b5d8
Aleks <aleksa.rab@gmail.com>
2015-04-10 23:37:55 +0200

    Bug fixes

--- Driver/FuelResourceGroupDriver.php
+++ Driver/FuelResourceGroupDriver.php
@@ -7,7 +7,7 @@ class FuelResourceGroupDriver implements ResourceGroupDriverInterface
     public function load($resource_group_name, $lang_name = null, $file_path = null)
     {
         \Config::set('language', $lang_name);
-        return \Fuel\Core\Lang::load($resource_group_name, $resource_group_name, $lang_name);
+        return \Fuel\Core\Lang::load($resource_group_name, $resource_group_name, $lang_name, false, true);
     }
 
     public function save($resource_group_name, $arr_translations, $lang_name = null, $file_path = null)
